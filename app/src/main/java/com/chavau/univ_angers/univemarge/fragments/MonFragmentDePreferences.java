d6980bd7746f68312c37941c0f09974b48e35996
HANINI <hanini.yassine1992@gmail.com>
2019-11-02 18:50:18 +0100

    Correction des conditions de changements du mot de passe

--- app/src/main/java/com/chavau/univ_angers/univemarge/fragments/MonFragmentDePreferences.java
+++ app/src/main/java/com/chavau/univ_angers/univemarge/fragments/MonFragmentDePreferences.java
@@ -1,12 +1,10 @@
 package com.chavau.univ_angers.univemarge.fragments;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.support.v7.preference.PreferenceScreen;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +14,7 @@ import android.widget.Toast;
 import com.chavau.univ_angers.univemarge.R;
 
 
-public class MonFragmentDePreferences extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class MonFragmentDePreferences extends PreferenceFragmentCompat {
     EditTextPreference etp;
     CheckBoxPreference cbp;
     EditText oldPass;
@@ -26,10 +24,10 @@ public class MonFragmentDePreferences extends PreferenceFragmentCompat implement
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preference);
-
+        // Faire l'appel de l'EditText et puis le cacher
         etp = (EditTextPreference) findPreference("pref_sync3");
         etp.setVisible(false);
-
+        // Faire l'appel du CheckBox et puis faire le Test
         cbp = (CheckBoxPreference) findPreference("pref_sync2");
         cbp.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
@@ -50,30 +48,31 @@ public class MonFragmentDePreferences extends PreferenceFragmentCompat implement
     @Override
     public void onResume() {
         super.onResume();
-        validerBtn.setOnClickListener(new View.OnClickListener() {
 
+        // Traitement conçernant le code Pin
+        validerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String oldPassPref = etp.getText();
                 String oldPassSaisi = oldPass.getText().toString();
                 String newPassSaisi = newPass.getText().toString();
-                if (oldPassPref.equals(oldPassSaisi)) {
-                    if (newPassSaisi.isEmpty()) {
-                        Toast.makeText(getActivity(), "Veuillez saisir un nouveau code pin", Toast.LENGTH_LONG).show();
+                if (oldPassSaisi.isEmpty() || newPassSaisi.isEmpty()) {
+                    Toast.makeText(getActivity(), "Veuillez remplir les champs !", Toast.LENGTH_LONG).show();
+                } else {
+                    if (newPassSaisi.length() != 4) {
+                        Toast.makeText(getActivity(), "Votre nouveau code pin doit avoir 4 numéros !", Toast.LENGTH_LONG).show();
                     } else {
                         if (newPassSaisi.equals(oldPassSaisi)) {
                             Toast.makeText(getActivity(), "Votre nouveau code pin doit étre different du courant !", Toast.LENGTH_LONG).show();
                         } else {
-                            if (newPassSaisi.length() != 4) {
-                                Toast.makeText(getActivity(), "Votre nouveau code pin doit avoir 4 numéros !", Toast.LENGTH_LONG).show();
+                            if (!oldPassSaisi.equals(oldPassPref)) {
+                                Toast.makeText(getActivity(), "Ancien code pin incorrect , veuillez réssayer !", Toast.LENGTH_LONG).show();
                             } else {
                                 etp.setText(newPass.getText().toString());
                                 Toast.makeText(getActivity(), "Code pin modifié avec succès !", Toast.LENGTH_LONG).show();
                             }
                         }
                     }
-                } else {
-                    Toast.makeText(getActivity(), "Ancien code pin incorrect , veuillez réssayer !", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -82,6 +81,8 @@ public class MonFragmentDePreferences extends PreferenceFragmentCompat implement
     @Override
     public void onStart() {
         super.onStart();
+
+        //Affichage des EditText par apport au CheckBox
         initChangePasswordsComponents();
         if (cbp.isChecked()) {
             setVisibilityComponents(true, oldPass, newPass, validerBtn);
@@ -92,6 +93,12 @@ public class MonFragmentDePreferences extends PreferenceFragmentCompat implement
 
     }
 
+    /**
+     * Methode permet suivant un boolean d'afficher ou non les composants liés au traitement de changement de mot de passe
+     *
+     * @param isVisible
+     * @param tvComponents
+     */
     private void setVisibilityComponents(boolean isVisible, TextView... tvComponents) {
         if (isVisible) {
             tvComponents[0].setVisibility(View.VISIBLE);
@@ -104,32 +111,14 @@ public class MonFragmentDePreferences extends PreferenceFragmentCompat implement
         }
     }
 
+    /**
+     * Methode permet d'initialiser les composantes de traitements du code pin
+     */
     private void initChangePasswordsComponents() {
         oldPass = getActivity().findViewById(R.id.edtoldpin);
         newPass = getActivity().findViewById(R.id.edtnewpin);
         validerBtn = getActivity().findViewById(R.id.btnchange);
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-        PreferenceScreen pref_screen = getPreferenceScreen();
-        int count = pref_screen.getPreferenceCount();
-        for (int i = 0; i < count; i++) {
-            Preference p = pref_screen.getPreference(i);
-            if (p instanceof EditTextPreference) {
-                String input = sharedPreferences.getString(p.getKey(), "");
-                System.out.println("My input : " + input);
-                try {
-                    double value = Double.parseDouble(input);
-                    if (value < 0)
-                        System.out.println(value + " is negative");
-                    else
-                        System.out.println(value + " is possitive");
-                    p.setSummary(input);
-                } catch (NumberFormatException e) {
-                    System.out.println("String " + input + "is not a number");
-                }
-            }
-        }
-    }
+
 }
