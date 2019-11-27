08a0004d32059ceb7ab486242314060b30eb13d6
Venatum <vincentlequec@gmail.com>
2019-11-27 16:48:38 +0100

    Add NFC Musculation

--- app/src/main/java/com/chavau/univ_angers/univemarge/view/activities/Musculation.java
+++ app/src/main/java/com/chavau/univ_angers/univemarge/view/activities/Musculation.java
@@ -1,7 +1,21 @@
 package com.chavau.univ_angers.univemarge.view.activities;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +23,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import android.widget.Toast;
 import com.chavau.univ_angers.univemarge.R;
 import com.chavau.univ_angers.univemarge.view.adapters.AdapterMusculation;
 import com.chavau.univ_angers.univemarge.view.adapters.AdapterViewPager;
@@ -30,6 +45,21 @@ public class Musculation extends AppCompatActivity {
 
     private static String PERSONNELS_PRESENTS = "presences";
 
+    private final int MY_PERMISSIONS_REQUEST_NFC = 1;
+    /**
+     * L'adaptateur gérant l'intéraction avec le téléphone pour le NFC.
+     */
+    private NfcAdapter nfcAdapter;
+    /**
+     * Contient le son à utiliser lorsque quelqu'un ayant badger est accepté par la base de données.
+     */
+    public MediaPlayer mp_son_approuver;
+    /**
+     * Contient le son à utiliser lorsque quelqu'un ayant badge n'est pas dans la base de données.
+     */
+    public MediaPlayer mp_son_refuser;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,14 +77,58 @@ public class Musculation extends AppCompatActivity {
             presences = savedInstanceState.getParcelableArrayList(PERSONNELS_PRESENTS);
         }
         mdata = creerMuscuData(presences.size());
-        adaptermusculation = new AdapterMusculation(this,presences,mdata);
+        adaptermusculation = new AdapterMusculation(this, presences, mdata);
+
+        // RFID
+        mp_son_approuver = MediaPlayer.create(Musculation.this, R.raw.bonbadgeaccepter);
+        mp_son_refuser = MediaPlayer.create(Musculation.this, R.raw.mauvaisbadgenonaccepter);
+
+        /*
+         * Initialize NFCAdapter
+         */
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null) {
+            /*
+             * Matériel NFC absent
+             */
+            Toast.makeText(this, "NFC absent sur le téléphone, fermeture de l'application ", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!nfcAdapter.isEnabled()) {
+            /*
+             * Si le NFC n'est pas activé alors on ouvre le menu pour
+             * que l'utilisateur puisse activer le module NFC
+             */
+            Toast.makeText(this, "Il faut activer le NFC", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+        }
+
+        // Si les autorisations du NFC ont été refusés
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.NFC)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.NFC}, MY_PERMISSIONS_REQUEST_NFC);
+        }
+
+        // TODO: Suppression du son par default
+//        nfcAdapter.enableReaderMode(this,
+//                tag -> System.out.println(tag),
+//            NfcAdapter.FLAG_READER_NFC_A |
+//            NfcAdapter.FLAG_READER_NFC_B |
+//            NfcAdapter.FLAG_READER_NFC_F |
+//            NfcAdapter.FLAG_READER_NFC_V |
+//            NfcAdapter.FLAG_READER_NFC_BARCODE |
+//            NfcAdapter.FLAG_READER_NO_PLATFORM_SOUNDS,
+//            null);
 
     }
 
     public MusculationData creerMuscuData(int presences) {
         MusculationData m_data = new MusculationData(33);
-        m_data.setTempsMinimum(1,30);
-        m_data.setOccupation(presences,m_data.getCapacite());
+        m_data.setTempsMinimum(1, 30);
+        m_data.setOccupation(presences, m_data.getCapacite());
         return m_data;
     }
 
@@ -62,7 +136,7 @@ public class Musculation extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        adapterviewpager = new AdapterViewPager(mdata,this);
+        adapterviewpager = new AdapterViewPager(mdata, this);
         viewpager.setAdapter(adapterviewpager);
 
         tabLayout.setupWithViewPager(viewpager, true);
@@ -76,43 +150,186 @@ public class Musculation extends AppCompatActivity {
                 // Suppression d'un personnel et mise à jour du recyclerview
                 adaptermusculation.enlever(position);
                 // Mise à jour du viewpager
-                mdata.setOccupation(adaptermusculation.getItemCount(),mdata.getCapacite());
+                mdata.setOccupation(adaptermusculation.getItemCount(), mdata.getCapacite());
                 View v = viewpager.findViewWithTag("OcuppationValue");
                 TextView tv = v.findViewById(R.id.id_details_value);
                 tv.setText(mdata.getOccupation());
             }
         });
+
+        // RFID
+        Intent intent = new Intent(this, Musculation.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        IntentFilter[] intentFilters = new IntentFilter[]{
+                new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED),
+        };
+        String[][] techLists = new String[][]{
+                new String[]{
+                        NfcA.class.getName()
+                },
+        };
+
+        try {
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, techLists);
+        } catch (Exception e) {
+            // TODO
+            System.out.println(e.toString());
+        }
+
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelableArrayList(PERSONNELS_PRESENTS,presences);
+        outState.putParcelableArrayList(PERSONNELS_PRESENTS, presences);
     }
 
     public ArrayList<Personnel> creerPers() {
         ArrayList<Personnel> personnels = new ArrayList<>();
         Random rand = new Random();
-        personnels.add(new Personnel("ALLON","LEVY"));
-        personnels.add(new Personnel("BACARD","HUGO"));
-        personnels.add(new Personnel("BAKER","MATTHEW"));
-        personnels.add(new Personnel("BALWE","CHETAN"));
-        personnels.add(new Personnel("BELAIR","LUC"));
-        personnels.add(new Personnel("CEBALLOS","CESAR"));
-        personnels.add(new Personnel("FAVRE","CHARLES"));
-        personnels.add(new Personnel("BYSZEWSKI","DYLAN"));
-        personnels.add(new Personnel("CHEN","CHRISTIAN"));
-        personnels.add(new Personnel("FEHM","ARNO"));
-        personnels.add(new Personnel("GARCIA","LUIS"));
-        personnels.add(new Personnel("FARGUES","LAURENT"));
-        personnels.add(new Personnel("HERBLOT","MATHILDE"));
-        personnels.add(new Personnel("LI","WEN-WEI"));
+        personnels.add(new Personnel("ALLON", "LEVY"));
+        personnels.add(new Personnel("BACARD", "HUGO"));
+        personnels.add(new Personnel("BAKER", "MATTHEW"));
+        personnels.add(new Personnel("BALWE", "CHETAN"));
+        personnels.add(new Personnel("BELAIR", "LUC"));
+        personnels.add(new Personnel("CEBALLOS", "CESAR"));
+        personnels.add(new Personnel("FAVRE", "CHARLES"));
+        personnels.add(new Personnel("BYSZEWSKI", "DYLAN"));
+        personnels.add(new Personnel("CHEN", "CHRISTIAN"));
+        personnels.add(new Personnel("FEHM", "ARNO"));
+        personnels.add(new Personnel("GARCIA", "LUIS"));
+        personnels.add(new Personnel("FARGUES", "LAURENT"));
+        personnels.add(new Personnel("HERBLOT", "MATHILDE"));
+        personnels.add(new Personnel("LI", "WEN-WEI"));
 
-        for(Personnel p : personnels) {
+        for (Personnel p : personnels) {
             int heure = rand.nextInt(3);
             int minute = rand.nextInt(60);
-            p.setHeurePassee(heure,minute);
+            p.setHeurePassee(heure, minute);
         }
         return personnels;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_NFC:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    System.out.println("Granted");
+                } else {
+                    // permission denied
+                    System.out.println("denied");
+                    Toast.makeText(Musculation.this, "[NFC] Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    /**
+     * Quand une carte est lue et détectée cette méthode est lancé
+     *
+     * @param intent Contient les éléments de la carte NFC
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        /*
+         * Pour ne pas bloquer l'interface utilisateur, le traitement est lancé en tâche de fond.
+         * Une tâche asynchrone est lancé avec un l'intent de la carte en argument
+         */
+        new Musculation.TraitementAsynchrone().execute(intent);
+    }
+
+    /**
+     * Désactive l'option enableForegroundDispatch
+     * Il est nécessaire de le faire lorsque l'application est mise en pause.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    /**
+     * Méthode servant à récupérer l'identifiant d'une carte étudiante
+     *
+     * @param tag Il s'agit ici de l'identifiant de lecture de la carte qui va être
+     *            utilisé pour créer l'identifiant unique
+     * @return L'identifiant unique de la carte
+     */
+    private String dumpTagData(Tag tag) {
+        byte[] id = tag.getId();
+        return toReversedHex(id);
+    }
+
+    /**
+     * Méthode calculant l'identifiant d'une carte étudiante
+     *
+     * @param bytes id de lecture de la carte converti en octet
+     * @return l'identifiant de la carte qui correspond au code hexadéciaml inversé.
+     */
+    private String toReversedHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte aByte : bytes) {
+            int b = aByte & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b).toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Lorsqu'une carte est lue elle se fait dans le thread d'interface, hors il déconseillé de faire
+     * un quelconque traitement dans ce thread. Cette tâche asynchrone à pour but de faire le traitement
+     * de lecture dans un nouveau thread.
+     */
+    public class TraitementAsynchrone extends AsyncTask<Intent, Void, String> {
+
+        /**
+         * Traitement qui se fait dans un thread de tâche de fond, afin que le thread de
+         * l'interface utilisateur soit toujours utilisable
+         *
+         * @param intents Contient l'intent de la carte
+         * @return l'id de la carte étudiante
+         */
+        @Override
+        protected String doInBackground(Intent... intents) {
+            System.out.println("doInBackground");
+            /*
+             * Récupérer l'intent
+             */
+            Intent intent = intents[0];
+
+            Tag tagId = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG); //Récupération du Tag permettant d'avoir l'identifiant
+
+            String idEtudiant = dumpTagData(tagId);
+            /*
+             * Permet d'aller dans la méthode onPostExecute
+             */
+            return idEtudiant;
+        }
+
+
+        /**
+         * Méthode appliqué quand le numéro de la carte ayant badgé a finalement été obtenue,
+         * Créé la requête pour envoyer le numéro à la base de données puis affiche les informations
+         * relatifs à la requête, joue un son selon le réultat de la requête.
+         *
+         * @param s : contient le numéro de la carte de l'étudiant ayant badgé.
+         */
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            System.out.println("MIFARE=" + s);
+            Toast.makeText(Musculation.this, "MIFARE:\n" + s, Toast.LENGTH_LONG).show();
+
+//            if () mp_son_approuver.start(); else mp_son_refuser.start();
+        }
+    }
+
 }
